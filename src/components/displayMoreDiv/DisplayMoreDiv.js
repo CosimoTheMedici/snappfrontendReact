0ee05ae45c19d093440442423c2877e032863fd2
cosimo_de_medici <cosmasthuku4@gmail.com>
2022-09-01 10:47:25 +0300

    Update DisplayMoreDiv.js

--- src/components/displayMoreDiv/DisplayMoreDiv.js
+++ src/components/displayMoreDiv/DisplayMoreDiv.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 import './displayMoreDiv.scss';
 const DisplayMoreDiv = () => {
@@ -6,7 +7,9 @@ const DisplayMoreDiv = () => {
     <section className="section_main">
         <div className='section_main-button'>
             <button className='section_main-button-button'>
+                <Link to="/all/movies">
                 <span className='section_main-button-span'>view all</span>
+                </Link>
             </button>
         </div>
         
